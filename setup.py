4db4b1528399136357910a883dcd330bc5eb1daa
Martijn van Exel <m@rtijn.org>
2015-07-03 15:20:50 -0600

    new version for pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-  name = 'maproulette-api-wrapper',
-  packages = ['maproulette-api-wrapper'], # this must be the same as the name above
-  version = '0.1.4',
+  name = 'maproulette',
+  packages = ['maproulette'], # this must be the same as the name above
+  version = '0.1.1',
   description = 'A wrapper for the MapRoulette API',
   author = 'Martijn van Exel',
   author_email = 'm@rtijn.org',
   url = 'https://github.com/mvexel/maproulette-api-wrapper',
-  download_url = 'https://github.com/mvexel/maproulette-api-wrapper/archive/0.1.4.tar.gz',
+  download_url = 'https://github.com/mvexel/maproulette-api-wrapper/archive/0.1.1.tar.gz',
   keywords = ['maproulette'],
   classifiers = [],
 )
