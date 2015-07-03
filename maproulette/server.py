99f88746e712e0233a9f13a1dbd9d544d237a1c0
Martijn van Exel <m@rtijn.org>
2015-07-02 21:59:57 -0600

    better server

--- maproulette/server.py
+++ maproulette/server.py
@@ -7,15 +7,17 @@ class MapRouletteServer(object):
 
     MAPROULETTE_PRODUCTION_URL = 'http://maproulette.org/api'
     ENDPOINTS = {
-        'ping'      :       '/ping',
-        'challenges':       '/challenges'
+        'ping'      : '/ping',
+        'challenges': '/challenges',
+        'tasks'     : '/admin/challenge/{}/tasks'
     }
 
     base_url = ''
+    active_challenge = None
 
-    def __init__(self, url=MAPROULETTE_PRODUCTION_URL):
+    def __init__(self, url=MAPROULETTE_PRODUCTION_URL, challenge=None):
         self.base_url = url
-        print 'inited with base url {}'.format(self.base_url)
+        self.active_challenge = challenge
 
     def alive(self):
         response = requests.get(self.base_url + self.ENDPOINTS['ping'])
@@ -26,5 +28,13 @@ class MapRouletteServer(object):
     def challenges(self):
         response = requests.get(self.base_url + self.ENDPOINTS['challenges'])
         if not response.ok:
-            return False
+            return None
+        return response.json()
+
+    def tasks(self):
+        if not self.active_challenge:
+            return None
+        print self.base_url + self.ENDPOINTS['tasks'].format(self.active_challenge)
+        response = requests.get(
+            self.base_url + self.ENDPOINTS['tasks'].format(self.active_challenge))
         return response.json()
