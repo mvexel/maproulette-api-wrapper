c78fd05e1afbee35db3e387bd99bb1fb5a84a2b0
Martijn van Exel <m@rtijn.org>
2015-07-02 21:48:26 -0600

    plumbing

--- maproulette/server.py
+++ maproulette/server.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
 
-class MapRouletteServer(Object):
-	"""A MapRoulette server"""
+import requests
 
-    MAPROULETTE_PRODUCTION_URL = 'http://maproulette.org/'
+class MapRouletteServer(object):
+    """A MapRoulette server"""
+
+    MAPROULETTE_PRODUCTION_URL = 'http://maproulette.org/api'
+    ENDPOINTS = {
+        'ping'      :       '/ping',
+        'challenges':       '/challenges'
+    }
 
     base_url = ''
 
-	def __init__(self, url=MAPROULETTE_PRODUCTION_URL):
-		self.base_url = url
+    def __init__(self, url=MAPROULETTE_PRODUCTION_URL):
+        self.base_url = url
+        print 'inited with base url {}'.format(self.base_url)
+
+    def alive(self):
+        response = requests.get(self.base_url + self.ENDPOINTS['ping'])
+        if not response.ok:
+            return False
+        return True
+
+    def challenges(self):
+        response = requests.get(self.base_url + self.ENDPOINTS['challenges'])
+        if not response.ok:
+            return False
+        return response.json()
