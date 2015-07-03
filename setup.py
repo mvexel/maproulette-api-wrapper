7aeb9ff7fbc867eaf1d8aad1bfbd8784972fbf81
Martijn van Exel <m@rtijn.org>
2015-07-03 15:16:59 -0600

    tag update in setup

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'maproulette-api-wrapper',
   packages = ['maproulette-api-wrapper'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.4',
   description = 'A wrapper for the MapRoulette API',
   author = 'Martijn van Exel',
   author_email = 'm@rtijn.org',
   url = 'https://github.com/mvexel/maproulette-api-wrapper',
-  download_url = 'https://github.com/mvexel/maproulette-api-wrapper/archive/0.1.tar.gz',
+  download_url = 'https://github.com/mvexel/maproulette-api-wrapper/archive/0.1.4.tar.gz',
   keywords = ['maproulette'],
   classifiers = [],
 )
