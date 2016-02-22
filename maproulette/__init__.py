e3d246d81185026d9380987cc54d4255794dde07
Martijn van Exel <m@rtijn.org>
2016-02-22 11:40:04 -0700

    relative path fix

--- maproulette/__init__.py
+++ maproulette/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from server import MapRouletteServer
-from challenge import MapRouletteChallenge
-from task import MapRouletteTask
-from taskcollection import MapRouletteTaskCollection
+from .server import MapRouletteServer
+from .challenge import MapRouletteChallenge
+from .task import MapRouletteTask
+from .taskcollection import MapRouletteTaskCollection
