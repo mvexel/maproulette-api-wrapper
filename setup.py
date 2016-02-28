835278111afefed2beecf9716a033529304c548f
Martijn van Exel <m@rtijn.org>
2016-02-28 10:23:05 -0700

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='maproulette',
     packages=['maproulette'],  # this must be the same as the name above
-    version='0.2.8',
+    version='0.2.9',
     description='A wrapper for the MapRoulette API',
     author='Martijn van Exel',
     author_email='m@rtijn.org',
